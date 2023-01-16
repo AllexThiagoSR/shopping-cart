83b3ffd0bb9cf7dd1caffd5024302291e5f710e6
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-16 15:15:30 -0300

    Adiciona dois novos arquivos

---
+++ src/helpers/cartFuncs.js
@@ -0,0 +1,23 @@
+/**
+ * Função que retorna todos os itens do carrinho salvos no localStorage.
+ * @returns {Array} Itens de ids salvos do carrinho ou array vazio.
+ */
+export const getSavedCartIDs = () => {
+  
+};
+
+/**
+ * Função que adiciona um product ao carrinho.
+ * @param {string} id - ID do product a ser adicionado.
+ */
+export const saveCartID = (id) => {
+
+};
+
+/**
+ * Função que remove um product do carrinho.
+ * @param {string} id - ID do product a ser removido.
+ */
+export const removeCartID = (id) => {
+  
+};
