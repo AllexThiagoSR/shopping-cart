e63a31a4ca7bdc7098f426acdc2eb37cae98abfa
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-16 16:37:31 -0300

    Requisito 3: Lista os produtos na página

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const productsSec = document.querySelector('.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const products = await fetchProductsList('computador');
+
+productsSec.replaceChildren(...products.map(createProductElement));
