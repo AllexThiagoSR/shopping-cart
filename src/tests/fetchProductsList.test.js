0fee024b61eaa26a112105ae550f8534f5953747
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-16 15:49:04 -0300

    Implementa testes para a função fetchProductsList e implementa essa mesma função

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,15 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(fetchProductsList).toBeInstanceOf(Function);
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
 
+  it('fetchProductsList com o argumento \'computador\' é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    await expect(fetchProductsList('computador')).resolves.toEqual(computadorSearch);
+  });
+
+  it('fetchProductsList sem argumento, retorna um erro com a mensagem: \'Termo de busca não informado\'', async () => {
+    await expect(fetchProductsList()).rejects.toThrow('Termo de busca não informado');
   });
 
   // it('...', () => {
