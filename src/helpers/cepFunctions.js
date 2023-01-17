0dcbdb535d7a46c0c30de948d95a868d53557945
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-17 10:19:49 -0300

    Requisito 11: Começa a implementação de searchCep

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,13 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const address = await Promise.any([
+    fetch(`https://cep.awesomeapi.com.br/json/${cep}`),
+    fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`),
+  ]);
+  const { city, state } = await address.json();
+  console.log(`${city} - ${state}`);
 };
 
 export const searchCep = () => {
-  // seu código aqui
+  const inputValue = document.querySelector('.cep-input').value;
+  getAddress(inputValue);
 };
